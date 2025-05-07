374a0512ca4ebb48cb3b5cf1bc1ff06d963680a6
NoaBuceta <noabuceta@iesteis.net>
2025-05-07 14:20:45 +0200

    Update and rename P1_Noa.py to P4_Noa.py

--- programas/P1_Noa.py
+++
@@ -1,1 +0,0 @@
-primeira practica da maqueta
---
+++ programas/P4_Noa.py
@@ -0,0 +1,20 @@
+"""Noa Buceta Blnaco"""
+from microbit import *
+
+def mover_servo(angulo):
+pwm = int((angulo / 180) * 102 + 26)
+pin2.write_analog(pwm)
+
+
+angulo = 0
+mover_servo(angulo)
+sleep(1000)
+
+while True:
+if button_b.is_pressed():
+if angulo == 0:
+angulo = 90
+else:
+angulo = 0
+mover_servo(angulo)
+sleep(500)
